6701f5ed8fcf0b6c5aa41872f75f515dc520b014
SMajev <82297087+SMajev@users.noreply.github.com>
2022-02-25 23:35:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 from controller import Controller
 from GUI import GUI
 
-msg = """Wzorzec projektowy Mediator jest podobny do wzorca Observer, 
-w tym sensie, że opisuje sposób wykonywania żądania i jego obsługi.
-Wzorzec ten warto zastosować, gdy w aplikacji, znajdziemy wiele komponentów, 
-które w jakiś sposób się ze sobą komunikują."""
+msg = """“There must be some way out of here,” said the joker to the thief
+
+“There’s too much confusion, I can’t get no relief
+
+Businessmen, they drink my wine, plowmen dig my earth
+
+None of them along the line know what any of it is worth”
+
+
+
+“No reason to get excited,” the thief, he kindly spoke
+
+“There are many here among us who feel that life is but a joke
+
+But you and I, we’ve been through that, and this is not our fate
+
+So let us not talk falsely now, the hour is getting late”
+
+
+
+All along the watchtower, princes kept the view
+
+While all the women came and went, barefoot servants, too
+
+
+
+Outside in the distance a wildcat did growl
+
+Two riders were approaching, the wind began to howl										"""
 
 
 def main():
